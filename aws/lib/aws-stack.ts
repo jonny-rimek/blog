645a6dc18ba2aa83d6efba903f1b7e0dc87eb3e2
Jonny Rimek <j.rimek@protonmail.com>
2019-09-08 16:53:53 +0200

    Add route53 alias record for the cfn distr

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -2,62 +2,69 @@ import cdk = require('@aws-cdk/core');
 import s3deploy = require('@aws-cdk/aws-s3-deployment');
 import s3 = require('@aws-cdk/aws-s3');
 import cloudfront = require('@aws-cdk/aws-cloudfront');
+import route53 = require('@aws-cdk/aws-route53');
+import alias = require('@aws-cdk/aws-route53-targets');
 import { Bucket } from '@aws-cdk/aws-s3';
 
 
 export class AwsStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-	super(scope, id, props);
+		super(scope, id, props);
 
-	// The code that defines your stack goes here
-	const BlogBucket = new s3.Bucket(this, 'Blog', {
-		websiteIndexDocument: 'index.html',
-		publicReadAccess: true
-	});	
+		// The code that defines your stack goes here
+		const BlogBucket = new s3.Bucket(this, 'Blog', {
+			websiteIndexDocument: 'index.html',
+			publicReadAccess: true
+		});	
 
-	const LogBucket = Bucket.fromBucketName(this, 'LogBucket', 'cfnlogbucket-56454534');
+		const LogBucket = Bucket.fromBucketName(this, 'LogBucket', 'cfnlogbucket-56454534');
 
-	const distribution = new cloudfront.CloudFrontWebDistribution(this, 'Distribution', {
-		//TODO: activate logging in existing cfnlog bucket
-		originConfigs: [
-			{
-				s3OriginSource: {
-					s3BucketSource: BlogBucket
-			},
-				behaviors : [ 
-					{
-						isDefaultBehavior: true,
-						compress: true,
-					}
-				], 
+		const distribution = new cloudfront.CloudFrontWebDistribution(this, 'Distribution', {
+			originConfigs: [
+				{
+					s3OriginSource: {
+						s3BucketSource: BlogBucket
+				},
+					behaviors : [ 
+						{
+							isDefaultBehavior: true,
+							compress: true,
+						}
+					], 
 
+				}
+			],
+			errorConfigurations: [
+				{
+					errorCode: 403,
+					responseCode: 200,
+					responsePagePath: '/index.html'
+				},
+				{
+					errorCode: 404,
+					responseCode: 200,
+					responsePagePath: '/index.html'
+				},
+			],
+			loggingConfig: {
+				bucket: LogBucket,
+				prefix: 'jonny-rimek-blog'
 			}
-		],
-		errorConfigurations: [
-			{
-				errorCode: 403,
-				responseCode: 200,
-				responsePagePath: '/index.html'
-			},
-			{
-				errorCode: 404,
-				responseCode: 200,
-				responsePagePath: '/index.html'
-			},
-		],
-		loggingConfig: {
-			bucket: LogBucket,
-			prefix: 'jonny-rimek-blog'
-		}
-	});
+		});
 
+		new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+			source: s3deploy.Source.asset('../public'),
+			destinationBucket: BlogBucket,
+			distribution,
+		});
 
-	new s3deploy.BucketDeployment(this, 'DeployWebsite', {
-		source: s3deploy.Source.asset('../public'),
-		destinationBucket: BlogBucket,
-		distribution,
-		// destinationKeyPrefix: 'web/static' // optional prefix in destination bucket
-	});
+		const zone = new route53.PublicHostedZone(this, 'HostedZone', {
+			zoneName: 'jonny-rimek.com'
+		  });
 
+		new route53.ARecord(this, 'AliasRecord', {
+			zone,
+			target: route53.RecordTarget.fromAlias(new alias.CloudFrontTarget(distribution)),
+		});
 	}
 }
